639af5687bbc682869e666829f2ea20f90a6cbe9
hasan <ghazzawi95@gmail.com>
2016-02-26 21:44:03 -0500

    Header and footer
    Css and header and footer added

---
+++ templates/footer.php
@@ -0,0 +1,14 @@
+  </section>
+         <!-- FOOTER -->
+         <div id="footer-content" class="sticky-footer">
+            <footer>
+               <div class="line">
+                  <div class="s-12 l-6">
+                     <p>Copyright 2016, Aunt Rosie Pie and Preserves</p>
+                  </div>
+               </div>
+            </footer>
+         </div>
+      </div>
+   </body>
+</html>
