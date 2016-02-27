ca57bfc3c73088d60f37f897cf649278ba4af99e
theblackwidower <theblackwidower@noprestige.com>
2016-02-27 09:05:44 -0500

    Started About Page
    Built about page, and started writing stuff in it.

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include("templates/header.php"); ?>
+<h1>About Us</h1>
+<p>Aunt Rosie's Homemade Pies and Preserves has been making mince pies and vegetable preserves for the past 35 years.</p>
+<p>Known throughout Southern Ontario for having some of of the best pies, people love these pies and pies pies pies.</p>
+<p>Aunt Rosie's Homemade Pies also took the title role in <cite>Rhod Gilbert and the Award-Winning Mince Pie</cite>.</p>
+<p>Come up with more about pies.</p>
+<!--UNFINISHED-->
+<?php include("templates/footer.php"); ?>
