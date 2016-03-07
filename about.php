64b3f6998e9feda4c4d1f2c6e5a66bf622ef0666
theblackwidower <theblackwidower@noprestige.com>
2016-03-07 13:00:45 -0500

    Added DVD image
    Added an image to illustrate the Pie DVD.

--- about.php
+++ about.php
@@ -1,5 +1,6 @@
 <?php include("templates/header.php"); ?>
 <h1>About Us</h1>
+<img src="img/MincePieDVD.jpg" alt="Rhod Gilbert and the Award-Winning Mince Pie" style="float:right;width:200px;"/>
 <p>Aunt Rosie's Homemade Pies and Preserves has been making mince pies and vegetable preserves for the past 35 years.</p>
 <p>Known throughout Southern Ontario for having some of of the best pies, people love these pies and pies pies pies.</p>
 <p>Aunt Rosie's Homemade Pies also took the title role in <cite>Rhod Gilbert and the Award-Winning Mince Pie</cite>.</p>
