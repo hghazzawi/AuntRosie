b0af6552c0548e1fd65a8e8b4267286c5745e83e
Justin Estaris <JustinEstaris@users.noreply.github.com>
2016-03-09 11:17:26 -0500

    Added files via upload
    Added employee-login page

---
+++ employee-login.php
@@ -0,0 +1,10 @@
+<?php $title="Homepage"; include("templates/header.php"); ?>
+
+<form>
+   <h1>Employee Login</h1>
+   <p>Email Address:</p> <input type="email" name="email-address"><br>
+   <p>Password:</p><input type="password" name="password">
+   <input type="submit" value="submit">
+</form>
+
+<?php include("templates/footer.php"); ?>
