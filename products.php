9f11fbf3c3feaab650487b4402b11a51021cd711
theblackwidower <theblackwidower@noprestige.com>
2016-03-12 13:25:26 -0500

    Fixed product page
    added empty array, and made all references to field names lowercase.

--- products.php
+++ products.php
@@ -1,11 +1,11 @@
 <?php $title="Products"; include("templates/header.php");
 
-$product_data = pg_fetch_all(pg_execute($connection, "get_all_products"));
+$product_data = pg_fetch_all(pg_execute($connection, "get_all_products", Array()));
 echo '<div id="product_listing">';
 foreach($product_data as $this_product)
 {
-  echo '<a href="product_details.php?id='.$this_product['ProductCode'].'"><img src="" /><span>'.$this_product['ProductName'].'</span></a>';
-  //could use: GroupCode, ProductType, ProductImageSmall
+  echo '<a href="product_details.php?id='.$this_product['productcode'].'"><img src="" /><span>'.$this_product['productname'].'</span></a>';
+  //could use: groupcode, producttype, productimagesmall
 }
 echo '</div>';
 include("templates/footer.php");
