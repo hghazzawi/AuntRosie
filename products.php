980fc92f4fc1eb6b0edc65feb6ed17630bc4f4ed
theblackwidower <theblackwidower@noprestige.com>
2016-03-12 20:07:58 -0500

    added product image string to product listing

--- products.php
+++ products.php
@@ -4,8 +4,8 @@ $product_data = pg_fetch_all(pg_execute($connection, "get_all_products", Array()
 echo '<div id="product_listing">';
 foreach($product_data as $this_product)
 {
-  echo '<a href="product_details.php?id='.$this_product['productcode'].'"><img src="" /><span>'.$this_product['productname'].'</span></a>';
-  //could use: groupcode, producttype, productimgmall
+  echo '<a href="product_details.php?id='.$this_product['productcode'].'"><img src="'.$this_product['productimgsmall'].'" /><span>'.$this_product['productname'].'</span></a>';
+  //could use: groupcode, producttype,
 }
 echo '</div>';
 
@@ -19,7 +19,7 @@ echo '</div>';
     	<li>
     		<img src="img/pies/pie.jpg" />
     		<h3>Best PIE</h3>
-    		
+
 			<button href="product-details.php">View Details</button>
     	</li>
     	<li>
@@ -37,7 +37,7 @@ echo '</div>';
     		<h3>Best PIE</h3>
     		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
     	</li>
-    	
+
     </ul>
 
 <?php include("templates/footer.php"); ?>
