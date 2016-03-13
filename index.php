fa1c6e1d7f23e2124e5b8853da9f803b5db4448e
hasan <ghazzawi95@gmail.com>
2016-03-12 20:18:01 -0500

    Added Banner to homepage

--- index.php
+++ index.php
@@ -2,18 +2,82 @@
 $title="Homepage";
 include("templates/header.php"); ?>
 
-<!-- FIRST BLOCK -->
-<div id="first-block">
-    <div class="line">
-        <div class="margin-bottom">
-            <div class="margin">
-                <article class="s-12">
-                    <h1>Aunt rosie</h1>
-                   
-                </article>
-            </div>
-        </div>
-    </div>
+<br>
+
+<div  id="banner">
+	<img style="margin-left: auto ;margin-right: auto ;" src="img/banner.jpg"/>
+</div>
+
+<br>
+<hr style="border: 0; height: 0; 
+		   border-top: 1px solid rgba(0, 0, 0, 0.1); 
+		   border-bottom: 1px solid rgba(255, 255, 255, 0.3);">
+
+<div>
+	
+	<h3 style="text-align: center">Best Selling Pies</h3><br>
+	<ul class="rig columns-4">
+    	<li>
+    		<img src="img/pies/pie.jpg" />
+    		<h3>Best PIE</h3>
+    		
+			<a href="product-details.php"><button>View Details</button></a>
+    	</li>
+    	<li>
+    		<img src="img/pies/pie.jpg" />
+    		<h3>Best PIE</h3>
+    		
+			<a href="product-details.php"><button>View Details</button></a>
+    	</li>
+		<li>
+    		<img src="img/pies/pie.jpg" />
+    		<h3>Best PIE</h3>
+    		
+			<a href="product-details.php"><button>View Details</button></a>
+    	</li>
+		<li>
+    		<img src="img/pies/pie.jpg" />
+    		<h3>Best PIE</h3>
+    		
+			<a href="product-details.php"><button>View Details</button></a>
+    	</li>
+		
+    	
+    </ul>
+</div>
+<hr style="border: 0; height: 0; 
+		   border-top: 1px solid rgba(0, 0, 0, 0.1); 
+		   border-bottom: 1px solid rgba(255, 255, 255, 0.3);">
+<div>
+	<h3 style="text-align: center" >Best Selling Preserves</h3><br>
+	<ul class="rig columns-4">
+    	<li>
+    		<img src="img/pies/pie.jpg" />
+    		<h3>Best PIE</h3>
+    		
+			<a href="product-details.php"><button>View Details</button></a>
+    	</li>
+    	<li>
+    		<img src="img/pies/pie.jpg" />
+    		<h3>Best PIE</h3>
+    		
+			<a href="product-details.php"><button>View Details</button></a>
+    	</li>
+		<li>
+    		<img src="img/pies/pie.jpg" />
+    		<h3>Best PIE</h3>
+    		
+			<a href="product-details.php"><button>View Details</button></a>
+    	</li>
+		<li>
+    		<img src="img/pies/pie.jpg" />
+    		<h3>Best PIE</h3>
+    		
+			<a href="product-details.php"><button>View Details</button></a>
+    	</li>
+		
+    	
+    </ul>
 </div>
 
 <?php include("templates/footer.php"); ?>
